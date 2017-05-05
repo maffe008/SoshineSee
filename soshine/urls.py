78d35f5f2ddc5a2218ea9dedf6f6adc4f320b203
Maffee <maffe008@hotmail.com>
2017-05-05 23:25:39 +0800

    somap

--- soshine/urls.py
+++ soshine/urls.py
@@ -3,6 +3,7 @@ from soshine import views
 
 urlpatterns = [
     url(r'^sogis/$',views.sogis,name = 'sogis'),
+    url(r'^somap/(\d+)$',views.somap,name = 'somap'),
     url(r'^index/$',views.index,name = 'soshineindex'),
 ]
 
--- soshine/views.py
+++ soshine/views.py
@@ -7,6 +7,27 @@ def index(request):
     soshine_index = u'SoshineTech 首页'
     return render(request, 'index.html', {'soshine_index': soshine_index})
 
+
 def sogis(request):
-    gisname = u"SoshineScene - Maffee's GIS 欢迎使用！"
+    gisname = u"SoshineSeeScene - Maffee's GIS 欢迎使用！"
     return render(request, 'sogis.html', {'gisname': gisname})
+
+
+def somap(request, param1):
+    abc_id = param1
+    abc_name = abc_id_to_abc_name(abc_id)
+    return render(request, 'somap.html', {'abc_id':abc_id,'abc_name': abc_name})
+
+
+def abc_id_to_abc_name(argument):
+    switcher = {
+        '1': "呼伦贝尔",
+        '2': "重庆",
+        '3': "大连",
+        '4': "青岛",
+        '5': "博罗",
+        '6': "赵光",
+        '7': "新疆",
+        '8': "张家口",
+    }
+    return switcher.get(argument, "nothing")
